fa08ec761e36c5b3a5d53de0305a39c324a70bb9
oktaalshina <oktaalshinaarvaparahyta@mail.ugm.ac.id>
2026-09-08 21:35:43 +0700

    feat: add project management routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/projects', function () {
+    return 'Project List';
+});
+
+Route::get('/projects/create', function () {
+    return 'Create Project';
+});
