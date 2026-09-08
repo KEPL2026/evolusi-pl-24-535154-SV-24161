e696d26ccbe910d1451e3993f4e2d70449715adc
oktaalshina <oktaalshinaarvaparahyta@mail.ugm.ac.id>
2026-09-08 21:58:10 +0700

    feat: implement project management

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $projects = Project::all();
+
+        return view('projects.index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'required|string',
+            'progress' => 'required|integer|min:0|max:100',
+            'start_date' => 'nullable|date',
+            'notes' => 'nullable|string',
+        ]);
+
+        Project::create($validated);
+
+        return redirect('/projects');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $project = Project::findOrFail($id);
+
+        return view('projects.edit', compact('project'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'required|string',
+            'progress' => 'required|integer|min:0|max:100',
+            'start_date' => 'nullable|date',
+            'notes' => 'nullable|string',
+        ]);
+
+        $project = Project::findOrFail($id);
+
+        $project->update($validated);
+
+        return redirect('/projects');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $project = Project::findOrFail($id);
+
+        $project->delete();
+
+        return redirect('/projects');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/projects', function () {
-    return 'Project List';
-});
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::post('/projects', [ProjectController::class, 'store']);
 
 Route::get('/projects/create', function () {
-    return 'Create Project';
+    return view('projects.create');
 });
+
+Route::get('/projects/{id}/edit', [ProjectController::class, 'edit']);
+Route::put('/projects/{id}', [ProjectController::class, 'update']);
+Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
